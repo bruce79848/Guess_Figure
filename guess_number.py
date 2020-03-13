9905a3a47182cc68c906118f64c72e2a0c517923
Bruce <bruce79848@gmail.com>
2020-03-13 17:52:30 +0800

    Upload an extensive version

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1,100)
+t = 0
 while True:
 	num = input('Input a number from 1~100 to see how fast you make it to a correct guess: ')
 	num = int(num)
+	t = t + 1
 	if num == r:
 		print('Bingo!')
+		print('You`ve taken', t , 'guess(es).')
 		break
 	elif num > r:
-		print('It`s bigger than the answer, take another try.')
+		print('Too big!')
 	elif num < r:
-		print('It`s smaller than the answer, take another try.')
+		print('Too small!')
+	print('You`ve taken', t , 'guess(es).')
